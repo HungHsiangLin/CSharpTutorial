d623b4c7a55e44d5ce087b1d7dce9e8addcf5652
HongHsiang <HongHsiang.Lin@iisigroup.com>
2019-01-31 15:39:35 +0800

    Y

---
+++ Tutorial02_BasicStructure/Basic.cs
@@ -0,0 +1,42 @@
+// C#內部的基本架構
+using System;
+
+//命名空間(namespace)
+namespace YourNamespace
+{
+    //類別(class)
+    class YourClass
+    {
+    }
+
+    //結構(struct)
+    struct YourStruct
+    {
+    }
+    
+    //介面(interface)
+    interface IYourInterface 
+    {
+    }
+
+    //委派(delegate)
+    delegate int YourDelegate();
+
+    //列舉(enum)
+    enum YourEnum 
+    {
+    }
+    namespace YourNestedNamespace
+    {
+        struct YourStruct 
+        {
+        }
+    }
+    class YourMainClass
+    {
+        static void Main(string[] args) 
+        {
+            //Your program starts here...
+        }
+    }
+}
---
+++ Tutorial02_BasicStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial02_BasicStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
