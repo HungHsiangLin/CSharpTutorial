1627352ab87f650e727f70630f30e32e737d597a
HongHsiang <HongHsiang.Lin@iisigroup.com>
2019-01-31 15:24:38 +0800

    Y

---
+++ Tutorial01_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
